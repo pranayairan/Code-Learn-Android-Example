7ac47c0a8d5477e3709f05947f8f7e4ff21b1997
Pranay Airan <pranay.airan@iiitb.net>
2013-12-13 00:50:13 +0530

    Adding List Example
    Adding List Example

---
+++ CodeLearnListExample/src/org/codelearn/codelearnlistexample/ListViewWithSimpleAdapter.java
@@ -0,0 +1,38 @@
+package org.codelearn.codelearnlistexample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * @author Pranay Airan
+ * 
+ */
+
+public class ListViewWithSimpleAdapter extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_with_simple_adapter);
+        
+        String[] codeLearnChapters = new String[] { "Android Introduction","Android Setup/Installation","Android Hello World",
+        		"Android Layouts/Viewgroups","Android Activity & Lifecycle","Intents in Android"};
+        
+        ArrayAdapter<String> codeLearnArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, codeLearnChapters);
+        
+        ListView codeLearnLessons = (ListView)findViewById(R.id.listView1);
+        codeLearnLessons.setAdapter(codeLearnArrayAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.list_view_with_simple_adapter, menu);
+        return true;
+    }
+    
+}
